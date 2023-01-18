35d9ed91b5eca68eb9721bfbcc6483eb6d86bbff
noa-micro <121046810+noa-micro@users.noreply.github.com>
2023-01-18 20:14:18 +0100

    añadido sonar

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, 40)
+})
+input.onButtonPressed(Button.AB, function () {
+    wuKong.stopMotor(wuKong.MotorList.M1)
+})
+input.onButtonPressed(Button.B, function () {
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, -40)
+})
+let Ojos = 0
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    `)
 basic.forever(function () {
 	
+})
+loops.everyInterval(100, function () {
+    Ojos = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+    if (Ojos < 10 && Ojos > 1) {
+        wuKong.setAllMotor(-40, 0)
+        wuKong.setLightMode(wuKong.LightMode.BREATH)
+        wuKong.lightIntensity(100)
+        basic.pause(randint(200, 600))
+        wuKong.setLightMode(wuKong.LightMode.OFF)
+        wuKong.lightIntensity(0)
+        wuKong.setAllMotor(40, 0)
+    }
 })
